a458551e174188cba98f2236e94c9094bf7684d2
FAhtisham <ahteshamabbasi1996@gmail.com>
2021-07-19 11:26:25 +0200

    Better training of AE

---
+++ dataset.py
@@ -0,0 +1,152 @@
+from typing_extensions import final
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import numpy as np
+import collections
+
+from tqdm import tqdm
+
+
+class Nucleotides:
+  def __init__(self, seqs):
+    self.nuc_pairs= self.make_pairs(seqs)
+    print("total words in vocab", self.nuc_pairs)
+    self.encoding= {w:i for i,w in enumerate(self.nuc_pairs,1)}
+    self.decoding= {i:w for i,w in enumerate(self.nuc_pairs,1)}
+
+  
+  def make_pairs(self, seqs, clip=1):
+    nuc_pairs= collections.Counter()
+
+    for seq in tqdm(seqs):
+      nuc_pairs.update(seq)
+
+    # check why this statement is so important (84, remains same without it)
+    for nucs in list(nuc_pairs.keys()):
+      if nuc_pairs[nucs] < clip:
+        nuc_pairs.pop(nucs)
+
+    return list(sorted(nuc_pairs.keys()))
+
+
+  def size(self):
+    assert len(self.encoding) == len(self.decoding)
+    return len(self.encoding)
+
+  
+
+
+class Sequences(Dataset):
+    def __init__(self, seq_len=131):
+      self.seq_len= seq_len
+      print(self.seq_len)
+      self.seqs= self.convert_seqs_to_words(self.load_data())
+      self.seqs= self.get_size_specific_seqs(self.seqs)
+      self.nucleotides= Nucleotides(self.seqs)
+    
+
+    def read_fasta(self,fp):
+        name, seq = None, []
+        for line in fp:
+            line = line.rstrip()
+            if line.startswith(">"):
+                if name: yield (name, ''.join(seq))
+                name, seq = line, []
+            else:
+                seq.append(line)
+        if name: yield (name, ''.join(seq))
+    
+    def load_data(self):
+      sequences = []
+      # Reading FASTA fil
+      with open("/raid/ahtisham/permissive_enhancers","r") as fp:
+        for name, seq in self.read_fasta(fp):
+          sequences.append(seq)
+      print("Sequences Read Succesfully !!!!")
+      print("Total Raw Sequences: ",len(sequences))
+      return sequences
+    
+    def add_padding(self,seq, p_len):
+      seq = seq + ("P" * p_len)
+      return seq
+  
+  
+    def convert_seqs_to_words(self,sequences):
+      f_sequences = []
+      for i in range(len(sequences)):
+        temp = ""
+        str_ = sequences[i]
+        j=0
+        if len(str_)%3!=0:
+          n = len(str_)
+          while n % 3 != 0:
+            n+=1
+            str_= self.add_padding(str_,n-len(str_))
+        for k in range(0,len(str_)):
+          j+=1
+          if  j%3==0:
+            temp = temp + str_[k-2:j] + ' ' 
+          #j+=3
+        f_sequences.append(temp) 
+      f_sequences= [j.split() for j in f_sequences]
+      return f_sequences
+    
+    def encode(self, seq):
+      enc= self.nucleotides.encoding
+      a = np.array([enc.get(c) for c in seq])
+      return a
+    
+    
+    def get_size_specific_seqs(self, seqs):
+      final_sequences=[]
+      for i in range(len(seqs)):
+        if (len(seqs[i]) == 131):
+          final_sequences.append(seqs[i])
+        elif (len(seqs[i])>131):
+            new_seqs=self.break_seq_into_smaller_chunks(seqs[i])
+            for i in range(len(new_seqs)):
+                final_sequences.append(new_seqs[i])
+      print("Now the total seqs are:", len(final_sequences))
+      
+      # for i in range(len(final_sequences)):
+      #     if len(final_sequences[i])>131:
+      #         print(len(final_sequences[i]))
+      return final_sequences
+    
+    def break_seq_into_smaller_chunks(self,seq):
+        new_seqs=[]
+        num_new_seqs = int(len(seq)/131)
+        print(num_new_seqs)
+        start_index=0
+        final_index=131
+        if num_new_seqs >=2:
+            for i in range(num_new_seqs):
+                new_seqs.append(seq[start_index:final_index])
+                start_index+=131
+                final_index+=131
+        elif(num_new_seqs<2):
+            new_seqs.append(seq[0:final_index])
+                    
+        return new_seqs
+                
+                
+    
+    def __len__(self):
+      return len(self.seqs)
+
+    def __getitem__(self,i):
+      return torch.from_numpy(self.encode(self.seqs[i]))
+
+
+
+def load(batch_size, seq_len):
+  data= Sequences(seq_len)
+  return (DataLoader(data, batch_size, shuffle=True), data.nucleotides)
---
+++ model.py
@@ -0,0 +1,171 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import numpy as np
+import collections
+
+from tqdm import tqdm
+
+
+
+
+# class Autoencoder(nn.Module):
+#   def __init__(self, nuc_pair_size, embedding_dims, e_hidden_dims, bottleneck_dims, d_hidden_dims, seq_length, dropout_size = 0.1):
+#     super().__init__()
+#     nuc_pair_size+=1
+#     self.seq_length= seq_length
+#     # define the vars over here (layers, objects)
+#     self.embedding= nn.Embedding( nuc_pair_size, embedding_dims)
+#     self.rnn1= nn.LSTM(input_size= embedding_dims, hidden_size= e_hidden_dims)#, num_layers=2)#, bidirectional=True)
+#     self.fc0= nn.Linear(in_features = e_hidden_dims, out_features= 200)
+#     self.fc1= nn.Linear(in_features = 200, out_features= bottleneck_dims)
+#     self.a1= nn.ReLU(True)
+#     #self.a1= nn.Sigmoid(True)
+#     self.dropout= nn.Dropout(dropout_size)
+    
+#     self.fc02= nn.Linear(in_features = bottleneck_dims, out_features= 200)
+#     self.fc2= nn.Linear(in_features = 200, out_features= d_hidden_dims)
+#     self.rnn2= nn.LSTM(input_size= d_hidden_dims, hidden_size= d_hidden_dims, num_layers=2)#, bidirectional=True)
+#     self.fc3= nn.Linear(in_features= d_hidden_dims, out_features= nuc_pair_size)
+
+
+
+#   def encoder(self, x):
+#     print("encoder's x", x.size())
+#     x= self.embedding(x).permute(1,0,2)
+#     print("encoder's embedding", x.size())
+#     _,(hidden_states, _)= self.rnn1(x)
+#     print("encoder hidden ", hidden_states.size())
+#     lv= self.fc0(hidden_states)
+#     print("encoder's lv", lv.size())
+#     lv= self.fc1(lv) # latent vector
+#     print("encoder's lv", x.size())
+#     lv= self.dropout(lv)
+#     return lv
+
+  
+#   def decoder(self, lv):
+#     # import pdb 
+#     # pdb.set_trace()
+#     print("dencoder's lv", lv.size())
+#     lv=self.fc02(lv)
+#     print("dencoder's lv2", lv.size())
+#     lv= self.fc2(lv)
+#     print("dencoder's lv3", lv.size())
+#     #output, _= self.rnn2(lv.repeat(self.seq_length,1,1),(lv,lv))
+#     output, _= self.rnn2(lv)
+#     print("dencoder's output", output.size())
+#     output= output.permute(1,0,2)
+#     print("permute k bad",output.size())
+#     logits= self.fc3(output)
+#     print("dencoder's logits fc3", logits.size())
+    
+#     #print(logits.size())
+#     logits = logits.transpose(1,2)
+#     print("final logits: ",logits.size())
+    
+#     return torch.squeeze(logits)
+  
+#   def forward(self,x):
+#     lv= self.encoder(x)
+#     logits= self.decoder(lv)
+#     return (lv.squeeze(), logits)
+    
+class Autoencoder(nn.Module):
+    def __init__(self, nuc_pair_size, embedding_dims, e_hidden_dims, bottleneck_dims, d_hidden_dims, seq_length, dropout_size = 0):
+        super().__init__()
+        nuc_pair_size+=1
+        self.seq_length= seq_length
+        # define the vars over here (layers, objects)
+        self.embedding= nn.Embedding( nuc_pair_size, embedding_dims)
+        self.rnn1= nn.LSTM(input_size= embedding_dims, hidden_size= e_hidden_dims, num_layers= 2)
+        self.fc1= nn.Linear(in_features = e_hidden_dims, out_features= bottleneck_dims)
+        self.a1= nn.ReLU(True)
+        self.dropout= nn.Dropout(dropout_size)
+        self.fc2= nn.Linear(in_features = bottleneck_dims, out_features= d_hidden_dims)
+        self.rnn2= nn.LSTM(input_size= d_hidden_dims, hidden_size= d_hidden_dims, num_layers=2)
+        self.fc3= nn.Linear(in_features= d_hidden_dims, out_features= nuc_pair_size)
+    
+    def encoder(self, x):
+        print("encoder's x",x.size())
+        x= self.embedding(x).permute(1,0,2)
+        print("encoder's x after emedding",x.size())
+        _,(hidden_states, _)= self.rnn1(x)
+        print("encoder's hidd",hidden_states[1].size())
+        hidden_states = hidden_states[1]
+        hidden_states= hidden_states[None, :,:]
+        lv= self.fc1(hidden_states) # latent vector
+        print("encoder's lv",lv.size())
+        lv= self.dropout(lv)
+        print("encoder's lv",lv.size())
+        return lv
+    
+    def decoder(self, lv):
+        import pdb
+        pdb.set_trace*()
+        print("dencoder's lv",lv.size())
+        lv= self.fc2(lv)
+        print("dencoder's lv 1st fc",lv.size())
+        #output,_=self.rnn2(lv)
+        
+        output, _= self.rnn2(lv.repeat(self.seq_length,1,1),(lv,lv))
+        # output=output[1]
+        # output=output[None,:,:]
+        #output,_=self.rnn2(lv)
+        print("dencoder's output rnn",output.size())
+        output= output.permute(1,0,2)
+        print("dencoder's permute",output.size())
+        logits= self.fc3(output)
+        print("dencoder's logits",logits.size())
+        return logits.transpose(1,2)
+    
+    
+    def forward(self,x):
+        lv= self.encoder(x)
+        logits= self.decoder(lv)
+        return (lv.squeeze(), logits)   
+    
+    
+
+class GanBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+        self.nnet= nn.Sequential(
+        nn.Linear(block_dims, block_dims),
+        nn.ReLU(True),
+        nn.Linear(block_dims, block_dims)        
+        )
+        
+    def forward(self,x):
+        x=self.nnet(x) # why to add x
+        print()
+      
+class Generator(nn.Module):
+    def __init__(self, n_layers, block_dims):
+        super().__init__()
+        self.gnet= nn.Sequential(
+        *[GanBlock(block_dims) for _ in range(n_layers)]
+        )
+      
+    def forward(self,x):
+        x= self.gnet(x)
+        return x
+        
+class Critic(nn.Module):
+    def __init__(self, n_layers, block_dims):
+        super().__init__()
+        self.cnet= nn.Sequential(*[GanBlock(block_dims) for _ in range(n_layers)]
+        )
+    
+    def forward(self,x):
+        x= self.cnet(x)
+        return x
+    
